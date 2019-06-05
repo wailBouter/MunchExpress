58d59d218117ce9c607d14bfa580fdf5ec7f0404
unknown <ew_bouterbiat@esi.dz>
2019-06-05 21:23:46 +0200

    Setup Vue js with Laravel for the Menu component

---
+++ resources/js/modules/menu/MenuContainer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper menu__container">
+        <p>this is a container</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['items']
+}
+</script>
+
+<style>
+
+</style>
