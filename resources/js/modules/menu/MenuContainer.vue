cf796cc21c6c44bf7c910b2e33d6e65a9949090e
unknown <ew_bouterbiat@esi.dz>
2019-06-05 23:57:58 +0200

    Using computed properties to show menu items

--- resources/js/modules/menu/MenuContainer.vue
+++ resources/js/modules/menu/MenuContainer.vue
@@ -4,7 +4,15 @@
             <div class="col-md-8">
                 <card-component>
                     <template slot="title">My Menu Items</template>
-                    <template slot="body">Content will be here</template>
+                    <template slot="body">
+                        <div class="section">
+                            <multiselect 
+                            v-model="menu"
+                            :options="categories">
+                            </multiselect>
+                        </div>
+                        <MenuGroup :items = currentMenuItems></MenuGroup>
+                    </template>
                 </card-component>
             </div>
             <div class="col-md-4">
@@ -18,8 +26,37 @@
 </template>
 
 <script>
+import _ from 'lodash';
+import Multiselect from 'vue-multiselect';
+import MenuGroup from './MenuGroups';
 export default {
-    props:['items']
+
+    components: {
+        Multiselect, MenuGroup
+    },
+    
+    props:['items'],
+
+    created() {
+        _.forEach(this.items, (item, key) => {
+            this.categories.push(key);
+        });
+        this.menu = this.categories[0];
+    },
+
+    data() {
+        return {
+            menu: '',
+            categories: []
+        }
+    },
+
+    computed: {
+        currentMenuItems() {
+            return this.items[this.menu];
+        }
+    }
+
 }
 </script>
 
---
+++ resources/js/modules/menu/MenuGroups.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="section">
+    <div class="table table-hover">
+      <thead>
+        <tr>
+          <th>Name &amp; description</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, key) in items" :key="key">
+          <td>
+            <strong v-text="item.name"></strong>
+            <p v-text="item.description"></p>
+          </td>
+          <td>
+            <span v-text="item.price"></span>
+          </td>
+        </tr>
+      </tbody>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      props: ['items']
+  }
+
+</script>
+
+<style>
+
+</style>
